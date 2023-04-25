1d1fec03e9ea098a3f2c6814662ccb1be136e9e5
MattWolf777 <109434781+MattWolf777@users.noreply.github.com>
2023-04-25 11:43:30 +0200

    Add files via upload

---
+++ toGithub/main.py
@@ -0,0 +1,24 @@
+import pygame, sys
+from settings import *
+from game_data import level_1, level_2
+from level import Level
+
+# Pygame Setup
+pygame.init()
+
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+level = Level(level_2, screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill("black")
+    level.run()
+
+    pygame.display.update()
+    clock.tick(60)
